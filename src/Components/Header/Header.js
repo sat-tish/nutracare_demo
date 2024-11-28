9037ade6c3ce133fcc376052ef6f79328d9d162d
sat-tish <satish30yash@gmail.com>
2024-11-28 13:56:43 +0530

    corrections done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Header.css";
 import { Container, Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -9,21 +8,21 @@ import SelectionHome from "../SelectionHome/SelectionHome";
 
 function Header() {
   const headerList = [
-      {
+    {
       img: "url(Images/BackgroundImages/bg_1.jpg.webp)",
       class: "img1",
-      offset: 0
-      },
-      {
-        img: "url(Images/BackgroundImages/bg_2.jpg.webp)",
-        class: "img2",
-        offset:0
-      },
-      {
-        img: "url(Images/BackgroundImages/bg_3.jpg.webp)",
-        class: "img3",
-        offset:6
-      },
+      offset: 0,
+    },
+    {
+      img: "url(Images/BackgroundImages/bg_2.jpg.webp)",
+      class: "img2",
+      offset: 0,
+    },
+    {
+      img: "url(Images/BackgroundImages/bg_3.jpg.webp)",
+      class: "img3",
+      offset: 6,
+    },
   ];
   const settings = {
     dots: false,
@@ -35,50 +34,43 @@ function Header() {
     waitForAnimate: false,
     autoplay: true,
     autoplaySpeed: 4000,
-    arrows: false
+    arrows: false,
   };
-  const headerAllList = headerList.map((element, index)=>{
-    return(
+  const headerAllList = headerList.map((element, index) => {
+    return (
       <div key={index}>
-      <div 
-        className="header-img img1"
-        style={{
-          backgroundImage: element.img
-        }}
-      >
-        {/* <div className="overlay" /> */}
-        <Container>
-          <Row>
-            <Col md={9} lg={7} xl={{span:6, offset: element.offset}}>
-              <div className="header-inner d-flex flex-column justify-content-center gap-2 ps-3">
-                <h2>Welcome to Nutracare360 Inc.</h2>
-                <h1 className="mb-5">Nutracare Holistic way of Healing</h1>
-                
-              </div>
-            </Col>
-          </Row>
-        </Container>
+        <div
+          className="header-img img1"
+          style={{
+            backgroundImage: element.img,
+          }}
+        >
+          {/* <div className="overlay" /> */}
+          <Container>
+            <Row>
+              <Col md={9} lg={7} xl={{ span: 6, offset: element.offset }}>
+                <div className="header-inner d-flex flex-column justify-content-center gap-2 ps-3">
+                  <h2>Welcome to Nutracare360 Inc.</h2>
+                  <h1 className="mb-5">Nutracare Holistic way of Healing</h1>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </div>
-      </div>
-    )
-  })
-
+    );
+  });
 
   return (
     <>
-     
-        <Container fluid className="header-wrapper p-0">
-      
-       <div className="slider-container">
-              <Slider {...settings}>
-                    {headerAllList}
-               </Slider>
-             </div> 
-        <div className="header-position">
-          <SelectionHome/>
+      <Container fluid className="header-wrapper p-0">
+        <div className="slider-container">
+          <Slider {...settings}>{headerAllList}</Slider>
         </div>
-        </Container>
-     
+        <div className="header-position">
+          <SelectionHome />
+        </div>
+      </Container>
     </>
   );
 }
